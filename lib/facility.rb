3d849894cf072efe2c65b0bc2bce92ea53757a7d
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-05-13 16:28:17 -0600

    Feat: registered vehicles collection

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,22 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, 
+              :address, 
+              :phone, 
+              :services,
+              :registered_vehicles
 
   def initialize(data)
     @name = data[:name]
     @address = data[:address]
     @phone = data[:phone]
     @services = []
+    @registered_vehicles = []
   end
 
   def add_service(service)
     @services << service
   end
+
+  # TODO: service checker helper method or module that creates
+  # reg vehicles attribute?
 end
